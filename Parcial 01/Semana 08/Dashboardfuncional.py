cd10a2916ad0d3932d5d090e981f77beef1e5602
ldyasada-Amb <ld.yasacamaa@uea.edu.ec>
2026-01-30 09:14:54 -0500

    se hizo el cambio las carpetas para que funcione con las carpetas de mi trabajo, el dashboar si funcion lo prove

---
+++ Parcial 01/Semana 08/Dashboardfuncional.py
@@ -0,0 +1,2 @@
+print("El dashboard funcional se está ejecutando correctamente.")
+print("se cambio el nombre de las carpetas en el dashboard para que se ejecute con mis carpetas de los trabajos de la semana")
